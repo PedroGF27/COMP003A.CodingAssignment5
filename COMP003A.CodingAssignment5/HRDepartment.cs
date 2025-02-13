7f7fe4003f02fa1da08b58b6fd21c085286233a6
PedroGF27 <coolcatdopedog@gmail.com>
2025-02-12 19:58:14 -0800

    Created HRDepartment class to override GetDepartmentDetails

---
+++ COMP003A.CodingAssignment5/HRDepartment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.CodingAssignment5
+{
+    internal class HRDepartment : Department
+    {
+        public override void GetDepartmentDetails()
+        {
+            Console.WriteLine("Handles employee relations and recruitment.");
+        }
+    }
+}
