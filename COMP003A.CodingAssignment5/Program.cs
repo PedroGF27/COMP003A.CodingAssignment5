4e7a7dfc606823a8121f3335165fd024f85e3098
PedroGF27 <coolcatdopedog@gmail.com>
2025-02-12 18:14:51 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Author: Pedro Garcia
+//Course: COMP-003A
+//Faculty: Jonathan Cruz
+//Purpose: Employee Managment system demonstrating OOP principles in C#
+namespace COMP003A.CodingAssignment5
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
